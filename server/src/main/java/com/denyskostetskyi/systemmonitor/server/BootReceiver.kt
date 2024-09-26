0d429132c6efc4d57e3e4f2d5d94add299f959e3
Denys Kostetskyi <denys.kostetskyi@globallogic.com>
2024-09-26 12:33:13 +0300

    Made SystemMonitorService a foreground service

--- server/src/main/java/com/denyskostetskyi/systemmonitor/server/BootReceiver.kt
+++ server/src/main/java/com/denyskostetskyi/systemmonitor/server/BootReceiver.kt
@@ -3,14 +3,23 @@ package com.denyskostetskyi.systemmonitor.server
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.util.Log
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
             Log.d(TAG, "ACTION_BOOT_COMPLETED received. Starting SystemMonitorService")
-            val serviceIntent = SystemMonitorService.newIntent(context)
-            context.startService(serviceIntent) //TODO use startForegroundService
+            startSystemMonitorService(context)
+        }
+    }
+
+    private fun startSystemMonitorService(context: Context) {
+        val intent = SystemMonitorService.newIntent(context)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(intent)
+        } else {
+            context.startService(intent)
         }
     }
 
